5a366445c6c4cb1d09cb7b0958be1c034e797270
OrangeBacon <computer.backup.15@gmail.com>
2020-12-31 12:28:42 +0000

    re-organise

--- src/solve_door.rs
+++ src/solve_door.rs
@@ -4,9 +4,9 @@ pub fn solve_door() {
     let values = vec![2i32, 3, 5, 7, 9];
 
     println!("matching: {:?}", values);
-    println!("to equation '_ + _ * _^2 + _^3 - _ = 399'");
+    println!("to equation: _ + _ * _^2 + _^3 - _ = 399");
 
-    let results: Vec<_> = values
+    let result: Vec<_> = values
         .iter()
         .copied()
         .permutations(values.len())
@@ -16,12 +16,14 @@ pub fn solve_door() {
                 nums[0] + nums[1] * nums[2].pow(2) + nums[3].pow(3) - nums[4],
             )
         })
-        .filter(|(_, b)| *b == 399)
-        .next()
+        .find(|(_, b)| *b == 399)
         .unwrap()
         .0;
 
-    println!("{:?}", results);
+    println!(
+        "solution: {} + {} * {}^2 + {}^3 - {} = 399",
+        result[0], result[1], result[2], result[3], result[4]
+    );
 }
 
 // [9, 2, 5, 7, 3]
